67f8975b32e867ebee95c5b59c2821a5dcf2435b
Ruben Pahino <rubasodin18@gmail.com>
2025-03-03 12:01:13 +0000

    Improve authentication

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ set -e
 CONTAINER_UID=1000
 CONTAINER_GID=999
 
+DOCKER_AUTHENTICATION=""
+[ -n "${DOCKER_USER}" ] && DOCKER_AUTHENTICATION+="-Djib.from.auth.username=${DOCKER_USER} "
+[ -n "${DOCKER_PASS}" ] && DOCKER_AUTHENTICATION+="-Djib.from.auth.password=${DOCKER_PASS}"
+
 mvn jib:dockerBuild \
         -s ".ci/settings.xml" \
         -Djib.to.image="${CONTAINER}" \
@@ -12,4 +16,5 @@ mvn jib:dockerBuild \
         -Djib.extraDirectories.paths='/jib-files' \
         -Djib.extraDirectories.permissions='/app/entrypoint.sh'='500' \
         -Djib.container.entrypoint="/app/entrypoint.sh" \
+        ${DOCKER_AUTHENTICATION} \
         ${EXTRA_JIB_OPTIONS}
