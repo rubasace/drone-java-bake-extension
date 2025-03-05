6df9347820e5409f7d75b82b6cd91873cfd54b1a
Ruben Pahino <rubasodin18@gmail.com>
2025-03-05 19:44:59 +0100

    Use drone plugin settings on script

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM alpine:3.20.2@sha256:0a4eaa0eecf5f8c050e5bba433f58c052be7587ee8af3e8b3910ef9ab5fbe9f5
 
 ENV HOME=/root
-ENV CONTAINER_UID=1000
-ENV CONTAINER_GID=999
 
 RUN mkdir -p "${HOME}/.m2/repository"
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-set -e
+set -e  # Exit immediately on error
 
+# Default values for container UID/GID
+CONTAINER_UID="${PLUGIN_CONTAINER_UID:-1000}"
+CONTAINER_GID="${PLUGIN_CONTAINER_GID:-999}"
+
+# Ensure PLUGIN_CONTAINER is set
+if [[ -z "${PLUGIN_CONTAINER}" ]]; then
+  echo "Error: 'container' setting is required but not provided."
+  exit 1
+fi
+
+# Handle Docker authentication
 DOCKER_AUTHENTICATION=""
-[ -n "${DOCKER_USER}" ] && DOCKER_AUTHENTICATION+="-Djib.to.auth.username=${DOCKER_USER} "
-[ -n "${DOCKER_PASS}" ] && DOCKER_AUTHENTICATION+="-Djib.to.auth.password=${DOCKER_PASS}"
+[ -n "${PLUGIN_DOCKER_USER}" ] && DOCKER_AUTHENTICATION+="-Djib.to.auth.username=${PLUGIN_DOCKER_USER} "
+[ -n "${PLUGIN_DOCKER_PASS}" ] && DOCKER_AUTHENTICATION+="-Djib.to.auth.password=${PLUGIN_DOCKER_PASS}"
 
+# Process extra JIB options: remove commas and convert to space-separated string
+EXTRA_JIB_OPTIONS=""
+if [[ -n "${PLUGIN_EXTRA_JIB_OPTIONS}" ]]; then
+  EXTRA_JIB_OPTIONS="${PLUGIN_EXTRA_JIB_OPTIONS//,/ }"
+fi
+
+# Run Maven JIB build
 mvn jib:build \
-        -Djib.to.image="${CONTAINER}" \
-        -Djib.container.user="${CONTAINER_UID}":"${CONTAINER_GID}" \
+        -Djib.to.image="${PLUGIN_CONTAINER}" \
+        -Djib.container.user="${CONTAINER_UID}:${CONTAINER_GID}" \
         -Djib.extraDirectories.paths='/jib-files' \
         -Djib.extraDirectories.permissions='/app/entrypoint.sh'='555' \
         -Djib.container.entrypoint="/app/entrypoint.sh" \
