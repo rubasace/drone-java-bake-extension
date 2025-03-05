22e130a6f99381a167efd3a99071790b5304ca55
Ruben Pahino <rubasodin18@gmail.com>
2025-03-05 13:02:52 +0100

    Fix Docker credentials params

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ CONTAINER_UID=1000
 CONTAINER_GID=999
 
 DOCKER_AUTHENTICATION=""
-[ -n "${DOCKER_USER}" ] && DOCKER_AUTHENTICATION+="-Djib.from.auth.username=${DOCKER_USER} "
-[ -n "${DOCKER_PASS}" ] && DOCKER_AUTHENTICATION+="-Djib.from.auth.password=${DOCKER_PASS}"
+[ -n "${DOCKER_USER}" ] && DOCKER_AUTHENTICATION+="-Djib.to.auth.username=${DOCKER_USER} "
+[ -n "${DOCKER_PASS}" ] && DOCKER_AUTHENTICATION+="-Djib.to.auth.password=${DOCKER_PASS}"
 
 mvn jib:dockerBuild \
         -Djib.to.image="${CONTAINER}" \
