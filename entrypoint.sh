9cc2df30de6c86652ff3922f89403bb70c4a3f9b
Ruben Pahino <rubasodin18@gmail.com>
2025-03-05 13:13:01 +0100

    Run jib build instead of dockerBuild so it doesn't depend on the Docker Daemon

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ DOCKER_AUTHENTICATION=""
 [ -n "${DOCKER_USER}" ] && DOCKER_AUTHENTICATION+="-Djib.to.auth.username=${DOCKER_USER} "
 [ -n "${DOCKER_PASS}" ] && DOCKER_AUTHENTICATION+="-Djib.to.auth.password=${DOCKER_PASS}"
 
-mvn jib:dockerBuild \
+mvn jib:build \
         -Djib.to.image="${CONTAINER}" \
         -Djib.container.user="${CONTAINER_UID}":"${CONTAINER_GID}" \
         -Djib.extraDirectories.paths='/jib-files' \
