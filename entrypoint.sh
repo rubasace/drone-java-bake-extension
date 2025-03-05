e399256369b076549d197fbd35c8eba8d9be707c
Ruben Pahino <rubasodin18@gmail.com>
2025-03-05 19:16:18 +0100

    Try to fix permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ mvn jib:build \
         -Djib.to.image="${CONTAINER}" \
         -Djib.container.user="${CONTAINER_UID}":"${CONTAINER_GID}" \
         -Djib.extraDirectories.paths='/jib-files' \
+        -Djib.extraDirectories.permissions='/app/entrypoint.sh'='555' \
         -Djib.container.entrypoint="/app/entrypoint.sh" \
         ${DOCKER_AUTHENTICATION} \
         ${EXTRA_JIB_OPTIONS}
